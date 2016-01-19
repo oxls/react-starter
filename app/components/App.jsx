f7d11c47a3e391a5cac88911996703a8a0b7e85a
Darren Royle <darren@easyreadsystem.com>
2016-01-19 20:36:51 +0000

    Initial commit

---
+++ app/components/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+require('./App.css');
+
+export default class App extends Component{
+
+  render () {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
